26c4bd2c82a2a6200bee6bd98ccc1dcbf9440cb1
devops4sure <devops4sure@gmail.com>
2026-02-10 13:49:24 +0530

    Replace localhost apply application API with VITE_API_URL

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -56,7 +56,7 @@ export default function LoginPage({ onLoginSuccess }) {
 
     /* ================= USER / EMPLOYEE LOGIN ================= */
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`,{
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
